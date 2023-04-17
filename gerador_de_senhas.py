4cc774351011878af0b4ad34b8205f3c65fb1bc0
Franklin Albuquerque <126638424+franklin-albuquerque@users.noreply.github.com>
2023-04-17 01:32:40 -0300

    Fazendo algumas melhorias

--- gerador_de_senhas.py
+++ gerador_de_senhas.py
@@ -1,16 +1,17 @@
 from random import randint
 
 def gerar_senha():
-    senha = []; caracteres = []
+    senha = []
 
-    for indice in range(33, 127):
-        caracteres.append(chr(indice))
     for _ in range(21):
-        for caractere in caracteres[randint(0, len(caracteres)-1)]:
+        for caractere in chr(randint(33, 126)):
             senha.append(caractere)
 
     return ''.join(senha)
 
-if __name__ == '__main__':
+def main():
     senha = gerar_senha()
     print(senha)
+
+if __name__ == '__main__':
+    main()
